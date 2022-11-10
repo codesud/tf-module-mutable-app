35d3453d616f303ab211b96e27cae3f515593b7b
Sudeep S <sudeepdirect@gmail.com>
2022-11-10 20:37:51 +0530

    Updated tf files

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "INSTANCE_TYPE" {}
+variable "ENV" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "OD_INSTANCE_COUNT" {}
+variable "COMPONENT" {}
+variable "APP_PORT" {}
+variable "WORKSPATION_IP" {}
+variable "PROMETHEUS_IP" {}
+variable "APP_VERSION" {}
+variable "LB_TYPE" {}
